de50dd77def4a5bfcb94ff01f29b1d635edae2f5
canwei <wu8260@163.com>
2025-08-15 17:56:09 +0800

    update code

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { FormatResponseInterceptor } from './format-response.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors();
+
+  // 将目录设置为静态文件目录,这样能直接访问上传的图片
+  app.useStaticAssets('uploads', {
+    prefix: '/uploads'
+  });
+
+  // 响应内容的拦截器
+  app.useGlobalInterceptors(new FormatResponseInterceptor());
 
   // 参数校验
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseInterceptors, UploadedFile, BadRequestException } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto } from './dto/create-user.dto'; 
 import { UpdateUserDto } from './dto/update-user.dto';
 import { FindUserDto } from './dto/find-user.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+import * as path from 'path';
+import { storage } from 'src/my-file-storage';
 
 @Controller('user')
 export class UserController {
@@ -26,4 +30,28 @@ export class UserController {
   remove(@Body() findUserDto: FindUserDto) {
     return this.userService.remove(findUserDto.userid);
   }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file', {
+    dest: 'uploads',
+    // 指定怎么存储
+    storage: storage,
+    limits: {
+      fileSize: 1024 * 1024 * 3
+    },
+    fileFilter(req, file, callback) {
+      const extname = path.extname(file.originalname);
+      if(['.png', '.jpg', '.jpeg', '.gif', '.webp'].includes(extname)) {
+        // callback第一个参数是error，第二个参数是是否接收文件
+        callback(null, true);
+      } else {
+        callback(new BadRequestException('只能上传图片'), false);
+      }
+    }
+  }))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log('file', file);
+    return file.path.replace('\\', '/');
+  }
+
 }
